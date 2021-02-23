ef2e55671c583f00a99b59dd967d5359ba2c01f6
Oniityann <zynfab26@hotmail.com>
2021-02-23 23:35:41 +0800

    change description
    change index.js text

--- new-site/gatsby-config.js
+++ new-site/gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Design+Code`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@Mento`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- new-site/src/pages/index.js
+++ new-site/src/pages/index.js
@@ -8,8 +8,11 @@ function IndexPage() {
     <Layout>
       <SEO title="Home" />
       <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
+      <h1>Design and code React apps</h1>
+      <p>
+        Don’t skip design. Learn design and code, by building real apps with
+        React and Swift. Complete courses about the best tools.
+      </p>
       <Link to="/page-2/">Go to page 2</Link> <br />
     </Layout>
   )
