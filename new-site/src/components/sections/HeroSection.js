b64ac53f8ec50f62b3770b23ef8800df8afed15e
ZhengYinan <zynfab26@hotmail.com>
2021-02-24 20:32:54 +0800

    Update Code
    1. Reset CSS
    2. Add HeroSection.js

---
+++ new-site/src/components/sections/HeroSection.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+function HeroSection() {
+  return (
+    <>
+      <img src="/images/logos/logo.svg" alt="logo" />
+      <h1>Design and code React apps</h1>
+      <p>
+        Don’t skip design. Learn design and code, by building real apps with
+        React and Swift. Complete courses about the best tools.
+      </p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </>
+  )
+}
+
+export default HeroSection
--- new-site/src/pages/index.js
+++ new-site/src/pages/index.js
@@ -2,18 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
+import HeroSection from "../components/sections/HeroSection"
 
 function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Design and code React apps</h1>
-      <p>
-        Don’t skip design. Learn design and code, by building real apps with
-        React and Swift. Complete courses about the best tools.
-      </p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
+      <HeroSection />
     </Layout>
   )
 }
