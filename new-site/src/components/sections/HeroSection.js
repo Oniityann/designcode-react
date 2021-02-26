d4936dc53ef8850d83b91abdf0f35e87b540aa56
ZhengYinan <zynfab26@hotmail.com>
2021-02-26 16:58:46 +0800

    add link button
    add PurchaseButton.js

--- new-site/src/components/sections/HeroSection.js
+++ new-site/src/components/sections/HeroSection.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 import { H1, MediumText } from "../styles/TextStyle"
 import { themes } from "../styles/ColorStyle"
+import PurchaseButton from "../buttons/PurchaseButton"
 
 function HeroSection() {
   return (
@@ -18,8 +19,11 @@ function HeroSection() {
             Don’t skiDescription design. Learn design and code, by building real
             apps with React and Swift. Complete courses about the best tools.
           </Description>
+          <PurchaseButton
+            title="Start learning"
+            subtitle="120+ hours of video"
+          />
         </TextWrapper>
-        <Link to="/page-2/">Go to page 2</Link> <br />
       </ContentWrapper>
     </Wrapper>
   )
