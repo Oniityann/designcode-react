ad20a30b628b3eecaad793eed531f0664e18f057
ZhengYinan <zynfab26@hotmail.com>
2021-02-28 03:43:07 +0800

    Add more animations
    1. add animations to ring/icon/iconwrapper。
    2. update gatsby version to 2.32.8

--- new-site/src/components/buttons/PurchaseButton.js
+++ new-site/src/components/buttons/PurchaseButton.js
@@ -9,7 +9,7 @@ export default function PurchaseButton(props) {
     <Link to="/page-2">
       <Wrapper>
         <IconWrapper>
-          <Icon src="/images/icons/credit.svg" />
+          <Icon src="/images/icons/credit.svg" className="icon" />
           <Ring src="/images/icons/icon-ring.svg" />
         </IconWrapper>
         <TextWrapper>
@@ -36,7 +36,11 @@ const Wrapper = styled.div`
   // 会使文字也居中
   align-items: center;
   gap: 20px;
-  transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
 
   :hover {
     box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
@@ -44,6 +48,10 @@ const Wrapper = styled.div`
       inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
 
     transform: translateY(-3px);
+
+    .icon {
+      transform: scale(1.2);
+    }
   }
 `
 const Title = styled(Caption2)`
@@ -70,6 +78,10 @@ const IconWrapper = styled.div`
   // 将自身位于父视图居中
   justify-self: center;
   position: relative;
+
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg) brightness(150%) saturate(120%);
+  }
 `
 const TextWrapper = styled.div`
   display: grid;
@@ -79,4 +91,8 @@ const Ring = styled.img`
   position: absolute;
   top: -15px;
   left: -16px;
+
+  ${Wrapper}:hover & {
+    transform: rotate(30deg) scale(1.2) translate(1px, 1px);
+  }
 `
