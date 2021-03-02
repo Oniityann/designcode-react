ed1ccb1581bacbd31b13d5f739cb7956b7ac58e2
ZhengYinan <zynfab26@hotmail.com>
2021-03-02 20:12:18 +0800

    Add MenuButton.js
    1. Add MenuButton.js
    2. bug fixed

--- new-site/src/components/layout/Header.js
+++ new-site/src/components/layout/Header.js
@@ -2,19 +2,17 @@ import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 import { menuData } from "../../data/menuData"
+import MenuButton from "../buttons/MenuButton"
 
 export default function Header() {
   return (
     <Wrapper>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <MenuWrapper>
+      <Link to="/index">
+        <img src="/images/logos/logo.svg" alt="logo" />
+      </Link>
+      <MenuWrapper count={menuData.length}>
         {menuData.map((item, index) => (
-          <Link to={item.link} key={index}>
-            <MenuItem>
-              <img src={item.icon} alt={item.title} />
-              {item.title}
-            </MenuItem>
-          </Link>
+          <MenuButton item={item} key={index} />
         ))}
       </MenuWrapper>
     </Wrapper>
@@ -33,23 +31,6 @@ const Wrapper = styled.div`
 `
 const MenuWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, auto);
+  grid-template-columns: repeat(${props => props.count}, auto);
   gap: 30px;
-`
-const MenuItem = styled.div`
-  color: rgba(255, 255, 255, 0.7);
-  display: grid;
-  grid-template-columns: 24px auto;
-  align-items: center;
-  gap: 10px;
-  padding: 10px 20px;
-  border-radius: 10px;
-
-  transition: 0.5s ease-out;
-
-  :hover {
-    background: rgba(255, 255, 255, 0.1);
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
-      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
-  }
 `
