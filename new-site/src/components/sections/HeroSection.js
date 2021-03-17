24530043e195145e9747c0b5e500dd4861f856f6
ZhengYinan <zynfab26@hotmail.com>
2021-03-17 19:14:42 +0800

    Update HeroSection.js
    Add keyframes animation to TitleWrapper

--- new-site/src/components/sections/HeroSection.js
+++ new-site/src/components/sections/HeroSection.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import { H1, MediumText } from "../styles/TextStyle"
 import { themes } from "../styles/ColorStyle"
 import PurchaseButton from "../buttons/PurchaseButton"
@@ -33,6 +33,14 @@ function HeroSection() {
 
 export default HeroSection
 
+const animation = keyframes`
+  from { opacity: 0; transform: translateY(-10px); filter: blur(10px); }
+  to { opacity: 1; transform: translateY(0px); filter: blur(0px); }
+
+  /* 0% { opacity: 0; transform: translateY(-10px); filter: blur(10px); }
+  100% { opacity: 1; transform: translateY(0px); filter: blur(0px); } */
+`
+
 const Wrapper = styled.div`
   overflow: hidden;
 `
@@ -51,6 +59,21 @@ const TextWrapper = styled.div`
   max-width: 360px;
   display: grid;
   gap: 30px;
+
+  > * {
+    opacity: 0;
+    animation: ${animation} 1s 0.2s ease-in-out forwards;
+
+    :nth-child(1) {
+      animation-delay: 0s;
+    }
+    :nth-child(2) {
+      animation-delay: 0.2s;
+    }
+    :nth-child(3) {
+      animation-delay: 0.4s;
+    }
+  }
 `
 const Title = styled(H1)`
   color: ${themes.light.text1};
